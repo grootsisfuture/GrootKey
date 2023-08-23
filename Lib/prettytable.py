7dca635a0d7a8b6be9ba194d8419ef0b00812a0b
grootsisfuture <142961778+grootsisfuture@users.noreply.github.com>
2023-08-23 10:08:54 +0530

    prettytable.py

--- Lib/prettytable.py
+++ Lib/prettytable.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2009-2013, Luke Maurits <luke@maurits.id.au>
-# All rights reserved.
-# With contributions from:
-#  * Chris Clark
-#  * Klein Stephane
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# * The name of the author may not be used to endorse or promote products
-#   derived from this software without specific prior written permission.
-#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,7 +11,7 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
+#
 __version__ = "0.7.2"
 
 import copy
