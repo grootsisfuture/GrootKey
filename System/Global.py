49e32c5e3ea9116fddbe8ced4a4b156c76306a37
grootsisfuture <142961778+grootsisfuture@users.noreply.github.com>
2023-09-03 22:41:30 +0530

    Global.py

--- System/Global.py
+++ System/Global.py
@@ -3,5 +3,5 @@ import os
 MAINPATH 	= os.path.abspath(os.path.join(os.path.dirname(__file__)))
 VERSION 	= '1.0.1'
 GITHUB		= 'https://github.com/jaganpatel'
-CODER		= 'Groots'
+CODERS		= 'Groots'
 AGENTS 		= list()
